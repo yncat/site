04bf83487ef159d7b633c1e4e23fa45c095bd564
Kochi <c011610813@edu.teu.ac.jp>
2020-10-02 00:05:37 +0900

    公開情報ページ追加

--- app/Controller/disclose/index.php
+++ app/Controller/disclose/index.php
@@ -3,6 +3,13 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+$app->get('/disclose/', function (Request $request, Response $response) {
+    $data=array();
+
+    // Render index view
+    return $this->view->render($response, 'disclose/index.twig', $data);
+});
+
 $app->get('/disclose/policy', function (Request $request, Response $response) {
     $data=array();
 
