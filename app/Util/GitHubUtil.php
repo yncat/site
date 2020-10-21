e023a2f33222e88922a4e44f410a81c4887b0cd6
Kochi <c011610813@edu.teu.ac.jp>
2020-10-21 23:57:59 +0900

    GitHubからのリリース取得時、リダイレクト先にも認証ヘッダが渡ってしまい400エラーとなる問題を修正

--- app/Util/GitHubUtil.php
+++ app/Util/GitHubUtil.php
@@ -47,10 +47,20 @@ class GitHubUtil{
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($handle, CURLOPT_USERAGENT, "ACTLaboratory-webadmin");
 		curl_setopt($handle, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($handle, CURLOPT_FOLLOWLOCATION, false);
+		$content = curl_exec($handle);
+		$fileUrl = curl_getinfo($handle, CURLINFO_REDIRECT_URL);
+		curl_close($handle);
+
+		$header=array("Accept: application/octet-stream");
+		$handle = curl_init($fileUrl);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_USERAGENT, "ACTLaboratory-webadmin");
+		curl_setopt($handle, CURLOPT_HTTPHEADER, $header);
 		curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
 		$content = curl_exec($handle);
+
 		curl_close($handle);
 		return $content;
 	}
-
 }
