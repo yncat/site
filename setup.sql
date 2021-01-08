a61dd9281f045f83b489bd693e9b1707e5bdb823
Yukio Nozawa <personal@nyanchangames.com>
2021-01-08 13:22:55 +0000

    Fix insert order

--- setup.sql
+++ setup.sql
@@ -28,6 +28,25 @@ CREATE TABLE `members` (
 
 INSERT INTO `members` VALUES (1,'test','test@test.test','$2y$10$FHkVgcgYCiGHjygZkHxX/epO.hrDIoe275./.nChMr8sJZx9wYtwK','test cat!',NULL,NULL,'yncat',1593933185,0);
 
+DROP TABLE IF EXISTS `softwares`;
+CREATE TABLE `softwares` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` char(255) DEFAULT NULL,
+  `keyword` char(32) DEFAULT NULL,
+  `description` text NOT NULL,
+  `features` text,
+  `gitHubURL` char(255) NOT NULL,
+  `snapshotURL` char(255) DEFAULT NULL,
+  `staff` int(10) unsigned NOT NULL,
+  `flag` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `keyword` (`keyword`),
+  KEY `staff` (`staff`),
+  CONSTRAINT `softwares_ibfk_1` FOREIGN KEY (`staff`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `softwares` VALUES (1,'test','TES','just test','test\\ttest is awesome, cats are cute!','actlaboratory/site','https://github.com/actlaboratory/site',1,0);
+
 DROP TABLE IF EXISTS `software_versions`;
 CREATE TABLE `software_versions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -49,25 +68,6 @@ CREATE TABLE `software_versions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `software_versions` VALUES (1,1,0,0,0,'first version','https://',NULL,NULL,NULL,NULL,'2020-01-01',0);
-
-DROP TABLE IF EXISTS `softwares`;
-CREATE TABLE `softwares` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` char(255) DEFAULT NULL,
-  `keyword` char(32) DEFAULT NULL,
-  `description` text NOT NULL,
-  `features` text,
-  `gitHubURL` char(255) NOT NULL,
-  `snapshotURL` char(255) DEFAULT NULL,
-  `staff` int(10) unsigned NOT NULL,
-  `flag` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `keyword` (`keyword`),
-  KEY `staff` (`staff`),
-  CONSTRAINT `softwares_ibfk_1` FOREIGN KEY (`staff`) REFERENCES `members` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `softwares` VALUES (1,'test','TES','just test','test\\ttest is awesome, cats are cute!','actlaboratory/site','https://github.com/actlaboratory/site',1,0);
 
 DROP TABLE IF EXISTS `updaterequests`;
 CREATE TABLE `updaterequests` (
