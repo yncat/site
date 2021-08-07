f956f54b4cc6bff8282259d0fae044d94b212ca7
yamahubuki <itiro.ishino@gmail.com>
2021-08-07 20:46:14 +0900

    同日に複数のalphaバージョンをリリースすると最新でない版をアップデータが返してしまう場合があった不具合を修正

--- app/Model/Dao/Softwares.php
+++ app/Model/Dao/Softwares.php
@@ -10,24 +10,22 @@ class Softwares extends Dao{
 
 	function getLatest($keyword=null, int $excludeFlag=0){
 
-		$subQueryBuilder = new QueryBuilder($this->db);
-		$subQueryBuilder
+		$subQueryBuilder = (new QueryBuilder($this->db))
 			->select("t1.software_id, t1.major, t1.minor, t1.patch, t1.released_at, t1.updater_URL, t1.hist_text, t1.updater_hash")
 			->from("software_versions","t1")
 			->orderBy("t1.released_at","DESC")
 			->where("NOT EXISTS(
 				SELECT 1 FROM software_versions t2 WHERE t1.software_id=t2.software_id AND t1.major*1000000+t1.minor*1000+t1.patch < t2.major*1000000+t2.minor*1000+t2.patch
 			)");
-		$queryBuilder = new QueryBuilder($this->db);
-		$queryBuilder
+
+		$queryBuilder = (new QueryBuilder($this->db))
 			->select(
 				"A.id, A.title, A.keyword, A.description, A.gitHubURL, A.flag,".
 				"B.major, B.minor, B.patch,B.released_at, B.updater_URL, B.hist_text, B.updater_hash")
 			->from("softwares","A")
 			->join("A","(".$subQueryBuilder->getSQL().")","B","A.id=B.software_id")
-			->orderBy("B.released_at","DESC");
-
-		$queryBuilder->where("A.flag & $excludeFlag = 0");
+			->orderBy("B.released_at","DESC")
+			->where("A.flag & $excludeFlag = 0");
 
 		//キーワード指定の時は１件しかいらない
 		if($keyword!=null){
@@ -35,23 +33,19 @@ class Softwares extends Dao{
 				-> setParameter(":kwd", $keyword);
 		}
 
-
-		$query = $queryBuilder->execute();
-		return $query->FetchALL();
+		return $queryBuilder->execute()->FetchALL();
 	}
 
 	function getLatestAlpha(string $keyword, int $excludeFlag=0){
-		$subQueryBuilder = new QueryBuilder($this->db);
-		$subQueryBuilder
+		$subQueryBuilder = (new QueryBuilder($this->db))
 			->select("t1.software_id, t1.major, t1.minor, t1.patch, t1.released_at, t1.updater_URL, t1.hist_text, t1.updater_hash")
 			->from("software_alpha_versions","t1")
 			->orderBy("t1.released_at","DESC")
 			->where("NOT EXISTS(
-				SELECT 1 FROM software_versions t2 WHERE t1.software_id=t2.software_id AND t1.major*1000000+t1.minor*1000+t1.patch < t2.major*1000000+t2.minor*1000+t2.patch
+				SELECT 1 FROM software_alpha_versions t2 WHERE t1.software_id=t2.software_id AND t1.major*1000000+t1.minor*1000+t1.patch < t2.major*1000000+t2.minor*1000+t2.patch
 			)");
 
-		$queryBuilder = new QueryBuilder($this->db);
-		$queryBuilder
+		$queryBuilder = (new QueryBuilder($this->db))
 			->select(
 				"A.id, A.title, A.keyword, A.description, A.gitHubURL, A.flag,".
 				"B.major, B.minor, B.patch,B.released_at, B.updater_URL, B.hist_text, B.updater_hash")
@@ -62,9 +56,7 @@ class Softwares extends Dao{
 			->andWhere("A.keyword = :kwd")
 			->setParameter(":kwd", $keyword);
 
-
-		$query = $queryBuilder->execute();
-		return $query->FetchALL();
+		return $queryBuilder->execute()->FetchALL();
 	}
 
 	function getVersions(string $textVersion = "1.0.0",string $keyword = "", bool $isAlpha = False){
