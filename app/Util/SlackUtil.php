6ba79ea0b78979aa41c34eac7633f6411cfc351e
yamahubuki <itiro.ishino@gmail.com>
2021-08-30 23:51:47 +0900

    単純なテキスト以外もSlackに投稿できるよう修正

--- app/Util/SlackUtil.php
+++ app/Util/SlackUtil.php
@@ -1,31 +1,37 @@
 <?php
 namespace Util;
 
+use Slim\Http\Request;
+
 use Util\EnvironmentUtil;
 
-class SlackUtil{
 
-	static function notify(string $text){
-		self::send($text,getenv("SLACK_NOTIFY_URL"));
+class SlackUtil {
+
+	static function notify($body){
+		self::send($body,getenv("SLACK_NOTIFY_URL"));
 	}
 
-	static function daily(string $text){
-		self::send($text,getenv("SLACK_DAILY_URL"));
+	static function daily($body){
+		self::send($body,getenv("SLACK_DAILY_URL"));
 	}
 
-	private static function send(string $text, string $url){
+	public static function send($body, string $url){
 		$header=array(
 			"Content-Type: application/json",
 			"User-Agent: ACTLaboratory-webadmin"
 		);
 
-		if (!EnvironmentUtil::isProduct()){
-			$text = "[".EnvironmentUtil::getEnvName()."] ".$text;
+		if (is_string($body)){
+			if (!EnvironmentUtil::isProduct()){
+				$body = "[".EnvironmentUtil::getEnvName()."] ".$text;
+			}
+
+			$body = [
+				"text" => $text
+			];
 		}
 
-		$body = [
-			"text" => $text
-		];
 		$context = array(
 			"http" => array(
 				"method"  => "POST",
@@ -37,5 +43,20 @@ class SlackUtil{
 
 		$result=file_get_contents($url, false, stream_context_create($context));
 		$result = json_decode($result,true);
+		return $result;
+	}
+
+	public static function velifySlackRequest(Request $request){
+	    $timestamp = $request->getHeaderLine('x-slack-request-timestamp');
+    	$signature = $request->getHeaderLine('x-slack-signature');
+	    $requestBody = $request->getBody()->getContents();
+    	$secret = getenv("SLACK_SIGNING_SECRET");
+
+		//カーソル位置を先頭に戻しておく
+		$request->getBody()->rewind();
+
+	    $sigBasestring = 'v0:' . $timestamp . ':' . $requestBody;
+    	$hash = 'v0=' . hash_hmac('sha256', $sigBasestring, $secret);
+	    return $hash === $signature;
 	}
 }
