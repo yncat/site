9a750f8114b3eb4d55b201ced010eecba1045f66
yamahubuki <itiro.ishino@gmail.com>
2021-09-05 19:14:16 +0900

    fix misstake

--- dbdump.php
+++ dbdump.php
@@ -4,10 +4,6 @@ if(empty($_GET["password"]) || $_GET["password"] != getenv("SCRIPT_PASSWORD")){
 	exit("Invalid password\n");
 }
 
-var_dump($_ENV);
-phpinfo();
-exit();
-
 $cmd = "mysqldump {$_ENV['DB_NAME']} -u {$_ENV['DB_USER']} -p{$_ENV['DB_PASS']} --default-character-set=utf8mb4 
 --no-tablespaces -B {$_ENV['DB_NAME']}";
 $data = shell_exec($cmd);
