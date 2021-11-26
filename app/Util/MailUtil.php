65c60b6e0b2491a12f88d4350553bde4025b11ed
yamahubuki <38341927+yamahubuki@users.noreply.github.com>
2021-11-26 22:18:51 +0900

    fix typo (#97)

--- app/Util/MailUtil.php
+++ app/Util/MailUtil.php
@@ -6,6 +6,7 @@ use Slim\Views\Twig;
 
 class MailUtil{
 	private const DEFAULT_MAIL_FROM = "ACTLaboratory<support@actlab.org>";
+	private const DEFAULT_ENVELOPE_FROM = "sender@actlab.org";
 
 
 	public static function sendWithTemplate($mail_to, string $title, string $template_name, array $data = []){
@@ -15,10 +16,16 @@ class MailUtil{
 		self::send($mail_to, $title, $content);
 	}
 
-	private static function send(string $mail_to, string $title, string $content, $mail_from = self::DEFAULT_MAIL_FROM){
+	private static function send(
+				string $mail_to,
+				string $title,
+				string $content,
+				$mail_from = self::DEFAULT_MAIL_FROM,
+				$envelope_from = self::DEFAULT_ENVELOPE_FROM
+			){
 		mb_language("Japanese");
 		mb_internal_encoding("UTF-8");
 		$header = "From: " . $mail_from;
-		mb_send_mail($mail_to, $title, $content,$header);
+		mb_send_mail($mail_to, $title, $content,$header,"-f " . $envelope_from);
 	}
 }
